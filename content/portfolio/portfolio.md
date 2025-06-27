d86f7aed1ac9f84635c46ae4fb838e4e3fd59880
PhatVo <9a10voletruongphat@gmail.com>
2025-06-28 01:21:26 +0700

    add portfolio beside blog

---
+++ content/portfolio/portfolio.md
@@ -0,0 +1,8 @@
+---
+title: "Dự án Landing Page"
+date: 2024-10-10
+description: "Một dự án thiết kế website landing page cho khách hàng công nghệ."
+image: ../../static/images/article-3.jpg
+---
+
+Nội dung chi tiết của dự án...
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,56 +1,14 @@
 import * as React from "react"
-import { graphql, Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 import { getImage, GatsbyImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = ({ data }) => (
+const BlogPage = ({ data }) => (
   <Layout>
-    <section className="hero my-5">
-      <div className="container ">
-        <div className="row">
-          <div className="col-6">
-            <div style={{ maxWidth: "450px" }} className="hero__content">
-              <h1 className="section-title" style={{ color: "#005243" }}>
-                Welcome to my blog!
-              </h1>
-              <p>
-                I use this space to document my adventures in front-end
-                development, sharing how I’ve overcome issues I’ve run into,
-                useful resources, and more.
-              </p>
-              <p>
-                Below are some of my favourite articles, or you can go to the
-                blog page to see my most recent posts.
-              </p>
-              <div className="flex-group">
-                <button
-                  style={{ backgroundColor: "#18846c", marginRight: "30px" }}
-                  className="btn text-white py-2 px-4"
-                >
-                  Featured articles
-                </button>
-                <button className="btn btn-light">Most recent articles</button>
-              </div>
-            </div>
-          </div>
-          <div className="col-6">
-            <div>
-              {/* <img src="//unsplash.it/510" alt className="hero__image" /> */}
-              <img
-                src="https://hoanghamobile.com/tin-tuc/wp-content/uploads/2024/11/tai-hinh-nen-dep-mien-phi.jpg"
-                alt=""
-                className="mg-fluid img-thumbnail"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-
     <section
-      style={{ backgroundColor: "#e9fbf8" }}
+      style={{ backgroundColor: "white" }}
       id="featured-articles"
       className="featured-articles p-5"
     >
@@ -110,14 +68,9 @@ const IndexPage = ({ data }) => (
   </Layout>
 )
 
-/**
- * Head export to define metadata for the page
- *
- * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
- */
-export const Head = () => <Seo title="Home" />
+export const Head = () => <Seo title="Blog" />
 
-export default IndexPage
+export default BlogPage
 
 export const query = graphql`
   query {
